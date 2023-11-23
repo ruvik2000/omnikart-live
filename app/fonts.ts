290a960f0c1990fc057c707fbfda88b2449f3674
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-23 22:34:35 +0530

    Added required fonts

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Baloo_Bhai_2, Inter } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const balooBhai2 = Baloo_Bhai_2({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
