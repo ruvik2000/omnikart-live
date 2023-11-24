23f849c85b430bc2715f215231f27f881295bad5
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-24 13:48:30 +0530

    links related to navigation bar

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const links = [
+  {
+    name: "Features",
+    hash: "/features",
+  },
+  {
+    name: "About Us",
+    hash: "/about",
+  },
+  {
+    name: "Contact Us",
+    hash: "/contact",
+  },
+];
