7889ef8cb480e0dc1bdbeed1edcd8daf79e20747
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-25 03:03:11 +0530

    Added cart info constant data

--- constants/index.ts
+++ constants/index.ts
@@ -12,3 +12,51 @@ export const links = [
     hash: "/contact",
   },
 ];
+
+export const cartInfo = [
+  {
+    id: 0,
+    imageUrl: "/side1.webp",
+    infoTitle: "Smart Drawer for Patient Safety",
+    infoList: [
+      "Eliminates the manual process of labelling drawers: Up to 16 patient-specific drawers can be assigned.",
+      "Independent locking drawers minimize the risk of administering the wrong medication to a patient.",
+      "Guided lights assist to identify the drawer to be opened.",
+      "Flexibility to configure workstations with storage drawers or bins.",
+      "Visual and sound alerts for drawer close reminders.",
+      "Manual unlocking access in emergencies.",
+    ],
+  },
+  {
+    id: 1,
+    imageUrl: "/side2.webp",
+    infoTitle:
+      "Provides a Faster, Safer and More Secure Medication Administration Process",
+    infoList: [
+      "Integrates the Omnicell ADC and Hospital Information System to enable a closed-loop process for medication tracking and management.",
+      "Secure transportation mode for medication from the ADC to the point-of-care, creating a critical layer of accountability.",
+      "Versatility allows it to double as a workstation which nurses can use for incorporating other clinical applications.",
+    ],
+  },
+  {
+    id: 2,
+    imageUrl: "/side3.webp",
+    infoTitle: "Improve Patient Care and Nurse Satisfaction",
+    infoList: [
+      "Capable of assigning separate or individual locking drawers for patients thus minimising trips to the ADC.",
+      "Enables quick retrieval of scheduled and as-needed medication.",
+      "Remote access allows nurses to review medication orders and manage medication requests.",
+      "Proven results: 77% of nurses were able to double-check medications due to time savings.",
+    ],
+  },
+  {
+    id: 3,
+    imageUrl: "/side4.webp",
+    infoTitle: "Workflow for Greater Efficiency",
+    infoList: [
+      "“My Patients” feature facilitates faster order processing.",
+      "Reduces time spent at the ADC",
+      "Proven results: saved at least 15min per nurse during the morning med pass; medication removal time at ADC decreased 33%.",
+    ],
+  },
+];
