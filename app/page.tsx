54ac7a3bfa81573308524c73f1a3dbe46860f130
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-26 02:15:13 +0530

    Converted home page to arrow function

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="flex flex-col">
-      {/* Banner Section */}
       <section className="relative h-screen w-full flex flex-col gap-3 items-start justify-center p-[2rem] md:p-[5rem]">
         <div className="z-10 bg-green-500/40 px-4 py-3 rounded-full -mt-16">
           <p className="text-xs md:text-sm font-semibold">
@@ -46,7 +45,6 @@ export default function Home() {
           className="absolute w-auto h-auto z-1 opacity-20 lg:opacity-100 md:block -right-0 bottom-0 object-cover"
         />
       </section>
-      {/* What is Section */}
       <section className="flex flex-col items-center justify-start gap-[2rem] px-5 md:px-[15rem] py-[5rem] bg-green-400 text-white">
         <h2 className="text-4xl font-semibold">What is OmniKart?</h2>
         <p className="text-center">
@@ -63,7 +61,6 @@ export default function Home() {
           quality of care.
         </p>
       </section>
-      {/* Mini Feature Section */}
       <section className="flex flex-col gap-10 py-[5rem] px-[2.5rem] md:px-[5rem] bg-neutral-100">
         {cartInfo.map((item) => (
           <React.Fragment key={item.id}>
@@ -73,4 +70,6 @@ export default function Home() {
       </section>
     </main>
   );
-}
+};
+
+export default Home;
