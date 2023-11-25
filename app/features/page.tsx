a946f0280cd427531185787c2f0ebec480962370
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-25 18:02:10 +0530

    Implemented sequence section with responsiveness

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -13,19 +13,19 @@ const page = () => {
           </React.Fragment>
         ))}
       </section>
-      <section className="flex flex-col gap-7 py-8 px-[10rem] text-center text-white bg-green-800">
+      <section className="flex flex-col gap-7 py-8 px-[2rem] lg:px-[10rem] text-center text-white bg-green-800">
         <h2 className="text-4xl font-semibold">
           Seamless Integration with Omnicell ADC
         </h2>
         <p>
           Streamlines the medication administration process and provides safe
           and secure transportation of medications from the medication cabinet
-          to the patient’s bedside. Nurses' shifts involve multiple trips to the
+          to the patient’s bedside. Nurse's shifts involve multiple trips to the
           Omnicell ADC & bedsides. Using the OmniKart eliminates the need for
           multiple trips by allowing nurses to request, retrieve, and deliver
           all patient's medication in a single trip.
         </p>
-        <div className="w-full mx-auto grid gap-4 grid-cols-1 grid-rows-8 md:grid-cols-2 md:grid-rows-4">
+        <div className="w-fit mx-auto grid gap-4 grid-cols-1 grid-rows-8 lg:grid-cols-2 lg:grid-rows-4">
           {sequenceList.map((item, index) => (
             <React.Fragment key={index}>
               <SequenceItem {...item} />
--- components/sequence-item.tsx
+++ components/sequence-item.tsx
@@ -7,7 +7,7 @@ type SequenceItemProps = {
 
 const SequenceItem = ({ number, description }: SequenceItemProps) => {
   return (
-    <div className="relative flex items-center justify-start bg-white w-[90%] text-black px-5 py-10 rounded-xl border-[3px] border-black">
+    <div className="relative flex items-center justify-start bg-white mx-auto w-[90%] text-black px-5 py-10 rounded-xl border-[3px] border-black">
       <div className="absolute -left-8 bg-white border-[3px] border-black w-14 h-14 flex items-center justify-center rounded-full">
         <span>{number}</span>
       </div>
