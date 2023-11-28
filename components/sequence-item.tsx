c5dc243dd6413c27614abd76a167e479fe476120
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-29 05:25:30 +0530

    Updated sequence item background

--- components/sequence-item.tsx
+++ components/sequence-item.tsx
@@ -7,11 +7,11 @@ type SequenceItemProps = {
 
 const SequenceItem = ({ number, description }: SequenceItemProps) => {
   return (
-    <div className="relative flex items-center justify-start bg-white mx-auto w-[90%] text-black px-5 py-10 rounded-xl border-[3px] border-black">
-      <div className="absolute -left-8 bg-white border-[3px] border-black w-14 h-14 flex items-center justify-center rounded-full">
-        <span>{number}</span>
+    <div className="relative flex items-center justify-start bg-white mx-auto w-[90%] text-black px-5 py-10 rounded-xl grad">
+      <div className="absolute -left-8 bg-white w-14 h-14 flex items-center justify-center rounded-full grad">
+        <span className="font-bold">{number}</span>
       </div>
-      <p className="ml-3 !text-left">{description}</p>
+      <p className="ml-3 !text-left font-semibold">{description}</p>
     </div>
   );
 };
