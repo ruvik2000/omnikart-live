5c3c802fb9f051c2dffb29ba6dfe09ab5becb11a
shanuka2000 <shanuka.inbox@gmail.com>
2023-12-01 23:50:46 +0530

    Updated sequence list color related to theme

--- components/sequence-item.tsx
+++ components/sequence-item.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useTheme } from "@/context/theme-context";
 import React from "react";
 
 type SequenceItemProps = {
@@ -6,12 +9,30 @@ type SequenceItemProps = {
 };
 
 const SequenceItem = ({ number, description }: SequenceItemProps) => {
+  const theme = useTheme();
+
   return (
-    <div className="relative flex items-center justify-start bg-white mx-auto w-[90%] text-black px-5 py-10 rounded-xl grad">
-      <div className="absolute -left-8 bg-white w-14 h-14 flex items-center justify-center rounded-full grad">
+    <div
+      className={`relative flex items-center justify-start  mx-auto w-[90%] text-black px-5 py-10 rounded-xl ${
+        theme.theme === "light" && `grad`
+      } dark:bg-[#1f1f1f] border dark:border-white`}
+    >
+      <div
+        className={`absolute -left-8  w-14 h-14 flex items-center justify-center rounded-full ${
+          theme.theme === "light" && `grad`
+        } dark:bg-[#1f1f1f] border dark:border-white dark:text-white`}
+      >
         <span className="font-bold">{number}</span>
+        <span className="font-bold hidden dark:block absolute dark:blur-lg">
+          {number}
+        </span>
       </div>
-      <p className="ml-3 !text-left font-semibold">{description}</p>
+      <div className="flex items-center justify-center dark:text-white">
+        <p className="ml-3 !text-left font-semibold">{description}</p>
+        <p className="hidden dark:block absolute ml-3 !text-left font-semibold blur-lg">
+          {description}
+        </p>
+      </div>
     </div>
   );
 };
